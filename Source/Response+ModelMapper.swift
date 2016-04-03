b66fd51856d63902af49724f606c91549a51a112
Łukasz Mróz <lukasz.mroz@droidsonroids.pl>
2016-04-03 17:17:42 +0200

    [Mapping] Use Mappers native map function for array

--- Source/Response+ModelMapper.swift
+++ Source/Response+ModelMapper.swift
@@ -12,7 +12,6 @@ import Mapper
 
 public extension Response {
     
-    /// Parse json (in ModelMapper it should be NSDictionary) to an object
     public func mapObject<T: Mappable>() throws -> T {
         guard let jsonDictionary = try mapJSON() as? NSDictionary, object = T.from(jsonDictionary) else {
             throw Error.JSONMapping(self)
@@ -30,19 +29,10 @@ public extension Response {
         return object
     }
     
-    /// Since 1.0.3 version of ModelMapper is not out in cocoapods,
-    /// we have to manually parse arrays
     public func mapArray<T: Mappable>() throws -> [T] {
-        guard let jsonArray = try mapJSON() as? NSArray else {
+        guard let jsonArray = try mapJSON() as? NSArray, object = T.from(jsonArray) else {
             throw Error.JSONMapping(self)
         }
-        
-        var returnArray = [T]()
-        for dict in jsonArray {
-            if let dict = dict as? NSDictionary, object = T.from(dict) {
-                returnArray.append(object)
-            }
-        }
-        return returnArray
+        return object
     }
 }
