2a4bfd2cd88ca634dcfc0a89564b1373aeabd6b8
Łukasz Mróz <lukasz.mroz@droidsonroids.pl>
2016-04-03 17:42:35 +0200

    [Mapping] Add mapArray(withKeyPath:) method

--- Source/Response+ModelMapper.swift
+++ Source/Response+ModelMapper.swift
@@ -16,6 +16,7 @@ public extension Response {
         guard let jsonDictionary = try mapJSON() as? NSDictionary, object = T.from(jsonDictionary) else {
             throw Error.JSONMapping(self)
         }
+        
         return object
     }
     
@@ -33,6 +34,17 @@ public extension Response {
         guard let jsonArray = try mapJSON() as? NSArray, object = T.from(jsonArray) else {
             throw Error.JSONMapping(self)
         }
+        
+        return object
+    }
+    
+    public func mapArray<T: Mappable>(withKeyPath keyPath: String) throws -> [T] {
+        guard let jsonDictionary = try mapJSON() as? NSDictionary,
+            let objectArray = jsonDictionary.valueForKeyPath(keyPath) as? NSArray,
+            let object = T.from(objectArray) else {
+                throw Error.JSONMapping(self)
+        }
+        
         return object
     }
 }
