c63e024cc5e5984de92d0026f0aaa6fc7f3240c8
Łukasz Mróz <lukasz.mroz@droidsonroids.pl>
2016-10-11 22:22:02 +0200

    [General] Fix RxSwift & ReactiveSwift for Swift 3.0

--- Source/ReactiveCocoa/SignalProducer+ModelMapper.swift
+++ Source/ReactiveCocoa/SignalProducer+ModelMapper.swift
@@ -6,17 +6,17 @@
 //  Copyright © 2016 sunshinejr. All rights reserved.
 //
 
-import ReactiveCocoa
+import ReactiveSwift
 import Moya
 import Mapper
 
 /// Extension for processing Responses into Mappable objects through ObjectMapper
-extension SignalProducerType where Value == Moya.Response, Error == Moya.Error {
+extension SignalProducerProtocol where Value == Moya.Response, Error == Moya.Error {
     
     /// Maps data received from the signal into an object which implements the Mappable protocol.
     /// If the conversion fails, the signal errors.
     public func mapObject<T: Mappable>(type: T.Type, keyPath: String? = nil) -> SignalProducer<T, Error> {
-        return producer.flatMap(.Latest) { response -> SignalProducer<T, Error> in
+        return producer.flatMap(.latest) { response -> SignalProducer<T, Error> in
             return unwrapThrowable { try response.mapObject(withKeyPath: keyPath) }
         }
     }
@@ -25,7 +25,7 @@ extension SignalProducerType where Value == Moya.Response, Error == Moya.Error {
     /// protocol.
     /// If the conversion fails, the signal errors.
     public func mapArray<T: Mappable>(type: T.Type, keyPath: String? = nil) -> SignalProducer<[T], Error> {
-        return producer.flatMap(.Latest) { response -> SignalProducer<[T], Error> in
+        return producer.flatMap(.latest) { response -> SignalProducer<[T], Error> in
             return unwrapThrowable { try response.mapArray(withKeyPath: keyPath) }
         }
     }
