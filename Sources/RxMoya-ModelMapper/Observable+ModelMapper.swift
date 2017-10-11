4af17ff44471429f6f6fa3a9ebb2ba3c6e789918
Łukasz Mróz <lukasz.mroz@droidsonroids.pl>
2017-10-11 20:57:33 +0200

    Remove schedulers from RxSwift map methods

--- Sources/RxMoya-ModelMapper/Observable+ModelMapper.swift
+++ Sources/RxMoya-ModelMapper/Observable+ModelMapper.swift
@@ -16,52 +16,44 @@ import Mapper
 
 /// Extension for processing Responses into Mappable objects through ObjectMapper
 public extension ObservableType where E == Response {
-    
-    /// Maps data received from the signal into an object (on the default Background thread) which
-    /// implements the Mappable protocol and returns the result back on the MainScheduler.
-    /// If the conversion fails, the signal errors.
+
+    /// Maps data received from the signal into an object which implement 
+    /// the Mappable protocol and returns the result back. If the conversion fails,
+    /// error event is sent.       
     public func map<T: Mappable>(to type: T.Type, keyPath: String? = nil) -> Observable<T> {
-        return observeOn(ConcurrentDispatchQueueScheduler(qos: .background))
-            .flatMap { response -> Observable<T> in
+        return flatMap { response -> Observable<T> in
                 return Observable.just(try response.map(to: type, keyPath: keyPath))
             }
-            .observeOn(MainScheduler.instance)
     }
     
-    /// Maps data received from the signal into an array of objects (on the default Background thread)
-    /// which implement the Mappable protocol and returns the result back on the MainScheduler
-    /// If the conversion fails, the signal errors.
+    /// Maps data received from the signal into an array of objects which implement 
+    /// the Mappable protocol and returns the result back. If the conversion fails,
+    /// error event is sent.   
     public func map<T: Mappable>(to type: [T].Type, keyPath: String? = nil) -> Observable<[T]> {
-        return observeOn(ConcurrentDispatchQueueScheduler(qos: .background))
-            .flatMap { response -> Observable<[T]> in
+        return flatMap { response -> Observable<[T]> in
                 return Observable.just(try response.map(to: type, keyPath: keyPath))
             }
-            .observeOn(MainScheduler.instance)
     }
-    
-    /// Maps data received from the signal into an object (on the default Background thread) which
-    /// implements the Mappable protocol and returns the result back on the MainScheduler.
-    /// If the conversion fails, the nil is returned instead of error signal.
+
+    /// Maps data received from the signal into an object which implement 
+    /// the Mappable protocol and returns the result back. If the conversion fails,
+    /// the nil is returned instead of error signal.
     public func mapOptional<T: Mappable>(to type: T.Type, keyPath: String? = nil) -> Observable<T?> {
-        return observeOn(ConcurrentDispatchQueueScheduler(qos: .background))
-            .flatMap { response -> Observable<T?> in
+        return flatMap { response -> Observable<T?> in
                 do {
                     let object = try response.map(to: type, keyPath: keyPath)
                     return Observable.just(object)
                 } catch {
                     return Observable.just(nil)
-                }
-                
+                }   
             }
-            .observeOn(MainScheduler.instance)
     }
     
-    /// Maps data received from the signal into an array of objects (on the default Background thread)
-    /// which implement the Mappable protocol and returns the result back on the MainScheduler
-    /// If the conversion fails, the nil is returned instead of error signal.
+    /// Maps data received from the signal into an array of objects which implement 
+    /// the Mappable protocol and returns the result back. If the conversion fails,
+    /// the nil is returned instead of error signal.
     public func mapOptional<T: Mappable>(to type: [T].Type, keyPath: String? = nil) -> Observable<[T]?> {
-        return observeOn(ConcurrentDispatchQueueScheduler(qos: .background))
-            .flatMap { response -> Observable<[T]?> in
+        return flatMap { response -> Observable<[T]?> in
                 do {
                     let object = try response.map(to: type, keyPath: keyPath)
                     return Observable.just(object)
@@ -69,6 +61,5 @@ public extension ObservableType where E == Response {
                     return Observable.just(nil)
                 }
             }
-            .observeOn(MainScheduler.instance)
     }
 }
