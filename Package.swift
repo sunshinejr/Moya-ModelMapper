55cc14b2cc8b4064593a1268713358f60560c127
Łukasz Mróz <lukasz.mroz@droidsonroids.pl>
2017-10-11 21:15:23 +0200

    Update SPM to PackageDescriptionV4

--- Package.swift
+++ Package.swift
@@ -1,16 +1,16 @@
-// swift-tools-version:3.1
+// swift-tools-version:4.0
 
 import PackageDescription
 
 let package = Package(
     name: "Moya-ModelMapper",
-    targets: [
-        Target(name: "Moya-ModelMapper"),
-        Target(name: "RxMoya-ModelMapper", dependencies: ["Moya-ModelMapper"]),
-        Target(name: "ReactiveMoya-ModelMapper", dependencies: ["Moya-ModelMapper"])
-    ],
     dependencies: [
-        .Package(url: "https://github.com/Moya/Moya", majorVersion: 9),
-        .Package(url: "https://github.com/lyft/mapper", majorVersion: 6),
-    ]
+        .package(url: "https://github.com/Moya/Moya.git", .exact("10.0.0-beta.1")),
+        .package(url: "https://github.com/lyft/mapper.git", .upToNextMajor(from: "7.0.0")),
+    ],
+    targets: [
+        .target(name: "Moya-ModelMapper", dependencies: ["Moya", "Mapper"]),
+        .target(name: "RxMoya-ModelMapper", dependencies: ["Moya-ModelMapper"]),
+        .target(name: "ReactiveMoya-ModelMapper", dependencies: ["Moya-ModelMapper"])
+    ]    
 )
