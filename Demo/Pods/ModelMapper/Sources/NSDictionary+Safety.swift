9af61650c05717210bfc486bf4bf62394ce8599f
Łukasz Mróz <lukasz.mroz@droidsonroids.pl>
2017-10-24 08:47:52 +0200

    Update Demo pods

--- Demo/Pods/ModelMapper/Sources/NSDictionary+Safety.swift
+++ Demo/Pods/ModelMapper/Sources/NSDictionary+Safety.swift
@@ -1,11 +1,12 @@
 import Foundation
 
 extension NSDictionary {
+    @nonobjc
     func safeValue(forKeyPath keyPath: String) -> Any? {
         var object: Any? = self
-        var keys = keyPath.characters.split(separator: ".").map(String.init)
+        var keys = keyPath.split(separator: ".").map(String.init)
 
-        while keys.count > 0, let currentObject = object {
+        while !keys.isEmpty, let currentObject = object {
             let key = keys.remove(at: 0)
             object = (currentObject as? NSDictionary)?[key]
         }
