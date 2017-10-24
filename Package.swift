dcc70307079336225d4609e0f71e90b0c280cdd1
Łukasz Mróz <lukasz.mroz@droidsonroids.pl>
2017-10-24 08:47:44 +0200

    Fix SPM integration for Moya 10

--- Package.swift
+++ Package.swift
@@ -5,12 +5,12 @@ import PackageDescription
 let package = Package(
     name: "Moya-ModelMapper",
     dependencies: [
-        .package(url: "https://github.com/Moya/Moya.git", .exact("10.0.0-beta.1")),
+        .package(url: "https://github.com/Moya/Moya.git", .upToNextMajor(from: "10.0.0")),
         .package(url: "https://github.com/lyft/mapper.git", .upToNextMajor(from: "7.0.0")),
     ],
     targets: [
         .target(name: "Moya-ModelMapper", dependencies: ["Moya", "Mapper"]),
-        .target(name: "RxMoya-ModelMapper", dependencies: ["Moya-ModelMapper"]),
-        .target(name: "ReactiveMoya-ModelMapper", dependencies: ["Moya-ModelMapper"])
+        .target(name: "RxMoya-ModelMapper", dependencies: ["RxMoya", "Mapper"]),
+        .target(name: "ReactiveMoya-ModelMapper", dependencies: ["ReactiveMoya", "Mapper"])
     ]    
 )
