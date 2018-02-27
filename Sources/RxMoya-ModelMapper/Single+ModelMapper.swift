f398e484e226a98734af10b3f3a54ef0fadba34c
Ivan Cheung <ivans.mailbox@gmail.com>
2018-02-27 18:01:07 +0900

    Single+ModelMapper: Add parameters to allow injection of a scheduler

--- Sources/RxMoya-ModelMapper/Single+ModelMapper.swift
+++ Sources/RxMoya-ModelMapper/Single+ModelMapper.swift
@@ -20,30 +20,28 @@ extension PrimitiveSequence where TraitType == SingleTrait, ElementType == Respo
     /// Maps data received from the signal into an object (on the default Background thread) which
     /// implements the Mappable protocol and returns the result back on the MainScheduler.
     /// If the conversion fails, the signal errors.
-    public func map<T: Mappable>(to type: T.Type, keyPath: String? = nil) -> Single<T> {
-        return observeOn(ConcurrentDispatchQueueScheduler(qos: .background))
+    public func map<T: Mappable>(to type: T.Type, scheduler: SchedulerType, keyPath: String? = nil) -> Single<T> {
+        return self.observeOn(scheduler)
             .flatMap { response -> Single<T> in
                 return Single.just(try response.map(to: type, keyPath: keyPath))
-            }
-            .observeOn(MainScheduler.instance)
+        }
     }
-
+    
     /// Maps data received from the signal into an array of objects (on the default Background thread)
     /// which implement the Mappable protocol and returns the result back on the MainScheduler
     /// If the conversion fails, the signal errors.
-    public func map<T: Mappable>(to type: [T].Type, keyPath: String? = nil) -> Single<[T]> {
-        return observeOn(ConcurrentDispatchQueueScheduler(qos: .background))
+    public func map<T: Mappable>(to type: [T].Type, scheduler: SchedulerType, keyPath: String? = nil) -> Single<[T]> {
+        return self.observeOn(scheduler)
             .flatMap { response -> Single<[T]> in
                 return Single.just(try response.map(to: type, keyPath: keyPath))
-            }
-            .observeOn(MainScheduler.instance)
+        }
     }
-
+    
     /// Maps data received from the signal into an object (on the default Background thread) which
     /// implements the Mappable protocol and returns the result back on the MainScheduler.
     /// If the conversion fails, the nil is returned instead of error signal.
-    public func mapOptional<T: Mappable>(to type: T.Type, keyPath: String? = nil) -> Single<T?> {
-        return observeOn(ConcurrentDispatchQueueScheduler(qos: .background))
+    public func mapOptional<T: Mappable>(to type: T.Type, scheduler: SchedulerType, keyPath: String? = nil) -> Single<T?> {
+        return self.observeOn(scheduler)
             .flatMap { response -> Single<T?> in
                 do {
                     let object = try response.map(to: type, keyPath: keyPath)
@@ -51,16 +49,14 @@ extension PrimitiveSequence where TraitType == SingleTrait, ElementType == Respo
                 } catch {
                     return Single.just(nil)
                 }
-
-            }
-            .observeOn(MainScheduler.instance)
+        }
     }
-
+    
     /// Maps data received from the signal into an array of objects (on the default Background thread)
     /// which implement the Mappable protocol and returns the result back on the MainScheduler
     /// If the conversion fails, the nil is returned instead of error signal.
-    public func mapOptional<T: Mappable>(to type: [T].Type, keyPath: String? = nil) -> Single<[T]?> {
-        return observeOn(ConcurrentDispatchQueueScheduler(qos: .background))
+    public func mapOptional<T: Mappable>(to type: [T].Type, scheduler: SchedulerType, keyPath: String? = nil) -> Single<[T]?> {
+        return self.observeOn(scheduler)
             .flatMap { response -> Single<[T]?> in
                 do {
                     let object = try response.map(to: type, keyPath: keyPath)
@@ -68,7 +64,7 @@ extension PrimitiveSequence where TraitType == SingleTrait, ElementType == Respo
                 } catch {
                     return Single.just(nil)
                 }
-            }
-            .observeOn(MainScheduler.instance)
+        }
     }
 }
+
