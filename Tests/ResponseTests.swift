4a17ac47f308d39331b5d788dd41841776a48ff6
Łukasz Mróz <lukasz.mroz@droidsonroids.pl>
2018-04-21 23:20:38 +0200

    Regenerate the project & add a testing spec file

---
+++ Tests/ResponseTests.swift
@@ -0,0 +1,18 @@
+//
+//  ResponseTests.swift
+//  Moya-ModelMapperTests
+//
+//  Created by Lukasz Mroz on 21.04.2018.
+//
+
+import Quick
+import Nimble
+
+final class ResponseTests: QuickSpec {
+
+    override func spec() {
+        it("should work"){
+            expect("this").to(equal("this"))
+        }
+    }
+}
