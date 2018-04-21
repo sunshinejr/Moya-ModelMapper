fb2fb767c665bb31d13238e112c28a4eb496c2c6
Łukasz Mróz <lukasz.mroz@droidsonroids.pl>
2018-04-22 01:12:37 +0200

    Make SPM work with test/build

--- Package.swift
+++ Package.swift
@@ -13,12 +13,14 @@ let package = Package(
         .package(url: "https://github.com/Moya/Moya.git", .upToNextMajor(from: "11.0.0")),
         .package(url: "https://github.com/lyft/mapper.git", .upToNextMajor(from: "7.0.0")),
         .package(url: "https://github.com/Quick/Quick.git", .upToNextMajor(from: "1.3.0")),
-        .package(url: "https://github.com/Quick/Nimble.git", .upToNextMajor(from: "7.0.3"))
+        .package(url: "https://github.com/Quick/Nimble.git", .upToNextMajor(from: "7.0.3")),
+        .package(url: "https://github.com/ReactiveCocoa/ReactiveSwift.git", .upToNextMajor(from: "3.0.0")),
+        .package(url: "https://github.com/ReactiveX/RxSwift.git", .upToNextMajor(from: "4.0.0"))
     ],
     targets: [
         .target(name: "Moya-ModelMapper", dependencies: ["Moya", "Mapper"]),
-        .target(name: "RxMoya-ModelMapper", dependencies: ["RxMoya", "Mapper"]),
-        .target(name: "ReactiveMoya-ModelMapper", dependencies: ["ReactiveMoya", "Mapper"]),
-        .testTarget(name: "Moya-ModelMapperTests", dependencies: ["Moya-ModelMapper", "Quick", "Nimble"])
+        .target(name: "RxMoya-ModelMapper", dependencies: ["Moya-ModelMapper", "RxMoya", "RxSwift", "Mapper"]),
+        .target(name: "ReactiveMoya-ModelMapper", dependencies: ["Moya-ModelMapper", "ReactiveMoya", "ReactiveSwift", "Mapper"]),
+        .testTarget(name: "Moya-ModelMapperTests", dependencies: ["Moya-ModelMapper", "RxMoya-ModelMapper", "ReactiveMoya-ModelMapper", "Quick", "Nimble", "ReactiveSwift", "RxSwift"])
     ]    
 )
