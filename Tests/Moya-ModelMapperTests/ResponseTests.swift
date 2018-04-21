c18fff54296e0c523bdc7a1d9a9d7c42539fc2c4
Łukasz Mróz <lukasz.mroz@droidsonroids.pl>
2018-04-22 00:01:38 +0200

    Add tests for compactMap

---
+++ Tests/Moya-ModelMapperTests/ResponseTests.swift
@@ -0,0 +1,78 @@
+//
+//  ResponseTests.swift
+//  Moya-ModelMapperTests
+//
+//  Created by Lukasz Mroz on 21.04.2018.
+//
+
+import Quick
+import Nimble
+import Mapper
+import Moya
+
+@testable import Moya_ModelMapper
+
+final class ResponseTests: QuickSpec {
+
+    override func spec() {
+        describe("compactMap") {
+            it("should map all objects as everything is mappable") {
+                let json = """
+                [
+                   {
+                      "test1":"test1",
+                      "test2":"test2"
+                   },
+                   {
+                      "test1":"test1",
+                      "test2":"test2"
+                   },
+                   {
+                      "test1":"test1"
+                   }
+                ]
+"""
+
+                let data = json.data(using: .utf8)!
+                let response = Response(statusCode: 200, data: data)
+                let mapped = try! response.compactMap(to: [Test].self)
+
+                expect(mapped.count) == 3
+            }
+
+            it("should map only two objects as one is not mappable") {
+                let json = """
+[
+   {
+      "test1":"test1",
+      "test2":"test2"
+   },
+   {
+      "test1":"test1",
+      "test2":"test2"
+   },
+   {
+      "test2":"test2"
+   }
+]
+"""
+
+                let data = json.data(using: .utf8)!
+                let response = Response(statusCode: 200, data: data)
+                let mapped = try! response.compactMap(to: [Test].self)
+
+                expect(mapped.count) == 2
+            }
+        }
+    }
+}
+
+struct Test: Mappable {
+    let test1: String
+    let test2: String?
+
+    public init(map: Mapper) throws {
+        try test1 = map.from("test1")
+        test2 = map.optionalFrom("test2")
+    }
+}
