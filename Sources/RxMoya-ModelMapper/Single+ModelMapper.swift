e52425a0bd2cca866c0c450d29685f784b276968
Łukasz Mróz <lukasz.mroz@droidsonroids.pl>
2018-04-22 00:39:21 +0200

    Add compactMap & compactMapOptional to Signal in RxSwift as well

--- Sources/RxMoya-ModelMapper/Single+ModelMapper.swift
+++ Sources/RxMoya-ModelMapper/Single+ModelMapper.swift
@@ -32,6 +32,15 @@ extension PrimitiveSequence where TraitType == SingleTrait, ElementType == Respo
                 return Single.just(try response.map(to: type, keyPath: keyPath))
         }
     }
+
+    /// Maps data received from the signal into an array of objects which implement the Mappable protocol.
+    /// If the conversion fails at any object, it's removed from the response array.
+    /// If you want to throw an error on any failure, use `map()` instead.
+    public func compactMap<T: Mappable>(to type: [T].Type, keyPath: String? = nil) -> Single<[T]> {
+        return flatMap { response -> Single<[T]> in
+            return Single.just(try response.compactMap(to: type, keyPath: keyPath))
+        }
+    }
     
     /// Maps data received from the signal into an object which implements the Mappable protocol.
     /// If the conversion fails, the nil is returned instead of error signal.
@@ -56,6 +65,21 @@ extension PrimitiveSequence where TraitType == SingleTrait, ElementType == Respo
                 } catch {
                     return Single.just(nil)
                 }
+        }
+    }
+
+    /// Maps data received from the signal into an array of objects which implement the Mappable protocol.
+    /// If the conversion fails at any object, it's removed from the response array.
+    /// If the whole conversion fails, the nil is returned instead of error signal.
+    /// Please see `map` and `compactMap` for other solutions to this problem.
+    public func compactMapOptional<T: Mappable>(to type: [T].Type, keyPath: String? = nil) -> Single<[T]?> {
+        return flatMap { response -> Single<[T]?> in
+            do {
+                let object = try response.compactMap(to: type, keyPath: keyPath)
+                return Single.just(object)
+            } catch {
+                return Single.just(nil)
+            }
         }
     }
 }
