1167c9011e8b235acfcbb435bd0bff3fee2e64f8
Łukasz Mróz <lukasz.mroz@droidsonroids.pl>
2018-04-22 00:39:04 +0200

    Add compactMap & compactMapOptional do RxSwift extension

--- Sources/RxMoya-ModelMapper/Observable+ModelMapper.swift
+++ Sources/RxMoya-ModelMapper/Observable+ModelMapper.swift
@@ -34,9 +34,15 @@ public extension ObservableType where E == Response {
             }
     }
 
-    /// Maps data received from the signal into an object which implement 
-    /// the Mappable protocol and returns the result back. If the conversion fails,
-    /// the nil is returned instead of error signal.
+    /// Maps data received from the signal into an array of objects which implement the Mappable protocol.
+    /// If the conversion fails at any object, it's removed from the response array.
+    /// If you want to throw an error on any failure, use `map()` instead.
+    public func compactMap<T: Mappable>(to type: [T].Type, keyPath: String? = nil) -> Observable<[T]> {
+        return flatMap { response -> Observable<[T]> in
+            return Observable.just(try response.compactMap(to: type, keyPath: keyPath))
+        }
+    }
+
     /// Maps data received from the signal into an object which implement the Mappable protocol.
     /// If the conversion fails, the nil is returned instead of error signal.
     public func mapOptional<T: Mappable>(to type: T.Type, keyPath: String? = nil) -> Observable<T?> {
@@ -61,5 +67,20 @@ public extension ObservableType where E == Response {
                     return Observable.just(nil)
                 }
             }
+    }
+
+    /// Maps data received from the signal into an array of objects which implement the Mappable protocol.
+    /// If the conversion fails at any object, it's removed from the response array.
+    /// If the whole conversion fails, the nil is returned instead of error signal.
+    /// Please see `map` and `compactMap` for other solutions to this problem.
+    public func compactMapOptional<T: Mappable>(to type: [T].Type, keyPath: String? = nil) -> Observable<[T]?> {
+        return flatMap { response -> Observable<[T]?> in
+            do {
+                let object = try response.compactMap(to: type, keyPath: keyPath)
+                return Observable.just(object)
+            } catch {
+                return Observable.just(nil)
+            }
+        }
     }
 }
