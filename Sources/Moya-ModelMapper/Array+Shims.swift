9260cc44af85b656169647dd4b1d6f5e64156f6a
Łukasz Mróz <thesunshinejr@gmail.com>
2019-10-07 16:29:06 +0200

    Remove Swift < 4.1 support, old availibility errors

--- Sources/Moya-ModelMapper/Array+Shims.swift
+++
@@ -1,8 +0,0 @@
-#if swift(>=4.1)
-#else
-extension Collection {
-    func compactMap<ElementOfResult>(_ transform: (Element) throws -> ElementOfResult?) rethrows -> [ElementOfResult] {
-        return try flatMap(transform)
-    }
-}
-#endif
